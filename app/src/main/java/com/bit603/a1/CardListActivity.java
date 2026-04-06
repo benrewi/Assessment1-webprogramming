e2f215733864f8bbd9744c58c7f37da94227fea3
BIT504 <ben.rewi@icloud.com>
2026-04-06 13:43:26 +1000

    Updates to items where errors existed

--- app/src/main/java/com/bit603/a1/CardListActivity.java
+++ app/src/main/java/com/bit603/a1/CardListActivity.java
@@ -10,21 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 public class CardListActivity extends AppCompatActivity {
+    private boolean isDevModeActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.card_list);
 
-        // 1. Get the data from the Intent (default is false)
         isDevModeActive = getIntent().getBooleanExtra("IS_DEV_MODE", false);
 
-        // 2. If it's active, apply the theme immediately
         if (isDevModeActive) {
             applyDeveloperTheme();
         }
 
-        // Setup your header title
         View header = findViewById(R.id.mainHeader);
         if (header != null) {
             ((TextView)header.findViewById(R.id.gameTitle)).setText("Card List");
@@ -32,10 +30,7 @@ public class CardListActivity extends AppCompatActivity {
     }
 
     private void applyDeveloperTheme() {
-        // Find your views in THIS activity and set them to Black/Grey
-        // (Just like your activateDeveloperMode method in MainActivity)
         findViewById(R.id.cardListRoot).setBackgroundColor(getColor(R.color.light_grey));
-        // etc...
     }
     }
-}
+
